437e013536750af4e5f01466909be1c3007d5a30
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-29 15:27:13 +0430

    add speakers to landing

--- src/views/Home/Components/SpeakersHome.vue
+++ src/views/Home/Components/SpeakersHome.vue
@@ -17,26 +17,22 @@
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
-          <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
-            alt=""
-            class="speakersImg h-100"
-          />
+          <img src="@/assets/Images/SpeakersMain.png" alt="" class="speakersImg h-100" />
           <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+            <div>Steve Bambury</div>
+            <p>Metaverse, Web3 and XR Consultant</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Thuraya Al Harthi- Speaker_ Digital Transformation_ Innovation.jpeg"
             alt=""
             class="speakersImg h-100"
           />
           <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+            <div>Thuraya Al Harthi</div>
+            <p>Speake, Digital Transformation, Innovation</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
@@ -44,37 +40,37 @@
       <div class="row no-gutters">
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Saba Al Busaidi- Senior Manager Corporate Affairs at Omantel .jpeg"
             alt=""
             class="speakersImg h-100"
           />
-          <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+          <div class="overlay2 text-white">
+            <div>Saba Al Busaidi</div>
+            <p>Senior Manager-Corporate Affairs - Omantel</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Aneela Hameed- Speaker _ Writer _ Marketing & Communication.jpeg"
             alt=""
             class="speakersImg h-100"
           />
-          <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+          <div class="overlay2 text-white">
+            <div>Aneela Hameed</div>
+            <p>Metaverse, Web3 and XR Consultant</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Hala Al Zadjali.jpeg"
             alt=""
             class="speakersImg h-100"
           />
-          <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+          <div class="overlay2 text-white">
+            <div>Hala Al Zadjali</div>
+            <p>Speaker</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
@@ -82,37 +78,37 @@
       <div class="row no-gutters">
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Maged Farrag-Creative and Managing Director at 5dVR.jpeg"
             alt=""
             class="speakersImg h-100"
           />
           <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+            <div>Maged Farrag</div>
+            <p>Creative and Managing Director at 5dVR</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Tariq Al Barwani-Award-Winning Technology Professional & Practical Motivational Speaker .jpeg"
             alt=""
             class="speakersImg h-100"
           />
           <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+            <div>Tariq Al Barwani</div>
+            <p>Award-Winning Technology Professional & Practical Motivational Speaker</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-4 col-sm-6 col-xs-6" id="speakersMain">
           <img
-            src="@/assets/Images/Screen Shot 2022-04-21 at 3.43.31 PM.png"
+            src="@/assets/Images/Tazima Al Ghannami- Senior Manager HR Analytics, Omantel.jpeg"
             alt=""
             class="speakersImg h-100"
           />
           <div class="overlay text-white">
-            <div>Sharifa Al Buruomi</div>
-            <p>Business Woman, Speaker</p>
+            <div>Tazima Al Ghannami</div>
+            <p>Senior Manager HR Analytics, Omantel</p>
             <router-link to="/speakers">More</router-link>
           </div>
         </div>
@@ -158,6 +154,7 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
 }
 .overlay2 {
   position: absolute;
@@ -170,6 +167,11 @@ export default {
   opacity: 0;
   transition: 0.5s ease;
   background-color: #e6007e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
 #speakersMain:hover .overlay {
@@ -193,7 +195,13 @@ export default {
   text-align: center;
 }
 
-.overlay a{
+.overlay a {
+  text-decoration: none;
+  color: #fff;
+  border: 1px solid #fff;
+  padding: 0.5rem 2rem;
+}
+.overlay2 a {
   text-decoration: none;
   color: #fff;
   border: 1px solid #fff;
