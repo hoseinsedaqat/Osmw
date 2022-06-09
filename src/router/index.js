3ee5ecbfd575faddc6015b0ce6b444155ba6b386
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-06-09 20:00:50 +0430

    add/hidden-all-routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,60 +9,56 @@ const routes = [
     name: 'home',
     component: () => import('@/views/Home/HomeView.vue')
   },
-  {
-    path: '/about-us',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About/AboutView.vue')
-  },
-  {
-    path: '/agenda',
-    name: 'Agenda',
-    component: () => import('@/views/Agenda/AgendaView.vue')
-  },
-  {
-    path: '/speakers',
-    name: 'Speakers',
-    component: () => import('@/views/Speakers/SpeakersView.vue')
-  },
-  {
-    path: '/sponsors',
-    name: 'Sponsors',
-    component: () => import('@/views/Sponsors/SponsorsView.vue')
-  },
-  {
-    path: '/media',
-    name: 'Media',
-    component: () => import('@/views/Media/MediaView.vue')
-  },
-  {
-    path: '/contact-us',
-    name: 'ContactUs',
-    component: () => import('@/views/Contact/ContactView.vue')
-  },
-  {
-    path: '/attend',
-    name: 'Attend',
-    component: () => import('@/views/Attend/AttendView.vue')
-  },
-  {
-    path:'/sponsorship',
-    name:'SponsorShip',
-    component: () => import('@/views/Sponsorship/SponsorshipView.vue')
-  },
-  // for Speakers
-  {
-    path:'/speaker/:id',
-    name:'SpeakerView',
-    component: () => import('@/views/Speaker/SpeakerView.vue')
-  },
-  {
-    path:'/thankyou',
-    name:'Thankyou',
-    component: () => import('@/components/ThankyouPage.vue')
-  }
+  // {
+  //   path: '/about-us',
+  //   name: 'about',
+  //   component: () => import('@/views/About/AboutView.vue')
+  // },
+  // {
+  //   path: '/agenda',
+  //   name: 'Agenda',
+  //   component: () => import('@/views/Agenda/AgendaView.vue')
+  // },
+  // {
+  //   path: '/speakers',
+  //   name: 'Speakers',
+  //   component: () => import('@/views/Speakers/SpeakersView.vue')
+  // },
+  // {
+  //   path: '/sponsors',
+  //   name: 'Sponsors',
+  //   component: () => import('@/views/Sponsors/SponsorsView.vue')
+  // },
+  // {
+  //   path: '/media',
+  //   name: 'Media',
+  //   component: () => import('@/views/Media/MediaView.vue')
+  // },
+  // {
+  //   path: '/contact-us',
+  //   name: 'ContactUs',
+  //   component: () => import('@/views/Contact/ContactView.vue')
+  // },
+  // {
+  //   path: '/attend',
+  //   name: 'Attend',
+  //   component: () => import('@/views/Attend/AttendView.vue')
+  // },
+  // {
+  //   path:'/sponsorship',
+  //   name:'SponsorShip',
+  //   component: () => import('@/views/Sponsorship/SponsorshipView.vue')
+  // },
+  // {
+  //   path:'/speaker/:id',
+  //   name:'SpeakerView',
+  //   component: () => import('@/views/Speaker/SpeakerView.vue')
+  // },
+  // {
+  //   path:'/thankyou',
+  //   name:'Thankyou',
+  //   component: () => import('@/components/ThankyouPage.vue')
+  // }
 ]
 
 const router = new VueRouter({
